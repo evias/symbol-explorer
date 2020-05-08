d51e06555d6b9a42433551444be9b4a98d6e9870
Gregory Saive <greg@evias.be>
2020-05-08 15:13:26 +0200

    fix url rewrites

--- server.js
+++ server.js
@@ -2,8 +2,16 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
+// configure server
 app = express();
 app.use(serveStatic(__dirname + "/www"));
+
+// url rewrite
+app.get('*', function(request, response, next) {
+    response.sendFile(__dirname + '/www/index.html')
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
